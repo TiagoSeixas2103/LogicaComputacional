af94b35de98242ff34c1a1d58b56d31b29fcbc84
Tiago Seixas <tvseixas@gmail.com>
2023-08-14 19:18:49 -0300

    esqueci do commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import sys
+
+valor = 0
+confirma_nulo = 0
+numero = 0
+simbolo = ""
+
+if sys.argv[1] == "''":
+    confirma_nulo = 1
+
+for i in range(len(sys.argv[1])):
+    #if sys.argv[1][i] != "+" and sys.argv[1][i] != "-" and sys.argv[1][i] != " " and sys.argv[1][i] != "'":
+    if sys.argv[1][i].isdigit():
+        if simbolo == "-":
+            valor += numero
+        else:
+            valor -= numero
+        numero = numero * 10
+        numero += int(sys.argv[1][i])
+
+    if simbolo == "-":
+        valor -= numero
+        numero = 0
+    else:
+        valor += numero
+        numero = 0
+    if sys.argv[1][i] != "+" or sys.argv[1][i] != "-":
+        simbolo = sys.argv[1][i]
+
+if not (confirma_nulo):
+    print(valor)
