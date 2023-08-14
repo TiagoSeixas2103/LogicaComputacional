ce70dbfeadc67669eb0a6ae20da7362e1c530748
Tiago Seixas <tvseixas@gmail.com>
2023-08-14 19:59:43 -0300

    corrigindo primeira

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import sys
 
 valor = 0
-confirma_nulo = 0
 numero = 0
 simbolo = ""
+confirma_nulo = 0
 
-if sys.argv[1] == "''":
+if sys.argv[1] == ' ':
     confirma_nulo = 1
 
 for i in range(len(sys.argv[1])):
@@ -23,9 +23,11 @@ for i in range(len(sys.argv[1])):
             valor += numero
 
     else:
-        if sys.argv[1][i] != "+" or sys.argv[1][i] != "-":
+        if sys.argv[1][i] == "+" or sys.argv[1][i] == "-":
             simbolo = sys.argv[1][i]
             numero = 0
 
 if not (confirma_nulo):
     print(valor)
+if (confirma_nulo):
+    print("erro")
