7a94edd411ded09538ad3c785a12bf2e13059fde
Tiago Seixas <tvseixas@gmail.com>
2023-08-14 19:40:37 -0300

    funcionando a principio

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ if sys.argv[1] == "''":
     confirma_nulo = 1
 
 for i in range(len(sys.argv[1])):
-    #if sys.argv[1][i] != "+" and sys.argv[1][i] != "-" and sys.argv[1][i] != " " and sys.argv[1][i] != "'":
     if sys.argv[1][i].isdigit():
         if simbolo == "-":
             valor += numero
@@ -17,15 +16,17 @@ for i in range(len(sys.argv[1])):
             valor -= numero
         numero = numero * 10
         numero += int(sys.argv[1][i])
+        print(numero)
+        
+        if simbolo == "-":
+            valor -= numero
+        else:
+            valor += numero
 
-    if simbolo == "-":
-        valor -= numero
-        numero = 0
     else:
-        valor += numero
-        numero = 0
-    if sys.argv[1][i] != "+" or sys.argv[1][i] != "-":
-        simbolo = sys.argv[1][i]
+        if sys.argv[1][i] != "+" or sys.argv[1][i] != "-":
+            simbolo = sys.argv[1][i]
+            numero = 0
 
 if not (confirma_nulo):
     print(valor)
