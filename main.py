805a927aec1481b9a69f6c17f4a069e12de5dbd6
Tiago Seixas <tvseixas@gmail.com>
2023-08-15 15:36:22 -0300

    v0.1.11

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import sys
 
 apareceu_numero = 0
+repete_simbolo = 0
 numero = 0
 simbolo = ""
 contagem = 0
+valor = 0
 
 for elemento in sys.argv[1]:
     if elemento.isdigit() and not contagem:
+        repete_simbolo = 0
         apareceu_numero = 1
         contagem = 1
-        valor = 0
 
     if elemento.isdigit():
+        repete_simbolo = 0
         if simbolo == "-":
             valor += numero
         else:
@@ -25,11 +28,16 @@ for elemento in sys.argv[1]:
             valor += numero
 
     else:
-        if (elemento == "+" or elemento == "-") and apareceu_numero:
+        if (elemento == "+" or elemento == "-") and apareceu_numero and elemento != "'":
+            if repete_simbolo == 1:
+                raise Exception('Simbolos consecutivos')
             simbolo = elemento
             numero = 0
+            repete_simbolo = 1
         elif (elemento == "+" or elemento == "-") and (not apareceu_numero or contagem == 0):
-            valor = "erro"
-            break
+            raise Exception('Nao teve numero precedendo simbolo, ou foi string vazia')
+
+if repete_simbolo == 1:
+    raise Exception('Terminou em simbolo')
 
 print(valor)
