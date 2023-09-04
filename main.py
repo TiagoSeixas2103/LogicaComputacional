96311d7718eb494db823d2a8fa546481f34473ef
TiagoSeixas2103 <tvseixas@gmail.com>
2023-09-04 17:12:21 -0300

    v1.2.0

--- main.py
+++ main.py
@@ -37,6 +37,12 @@ class Tokenizer:
             elif self.source[self.position] == "/":
                 self.next.type = "DIV"
                 self.next.value = None
+            elif self.source[self.position] == "(":
+                self.next.type = "OPENPAR"
+                self.next.value = None
+            elif self.source[self.position] == ")":
+                self.next.type = "CLOSEPAR"
+                self.next.value = None
             self.position += 1
         else:
             self.next.type = "EOF"
@@ -45,26 +51,39 @@ class Tokenizer:
 
 class Parser:
     tokenizer = Tokenizer("", 0, Token("", None))
-    def parseTerm():
+    def parseFactor():        
         if Parser.tokenizer.next.value != None:
-            num = Parser.tokenizer.next.value
+            resultado = Parser.tokenizer.next.value
             Parser.tokenizer.selectNext()
-            while Parser.tokenizer.next.type == "MULT" or Parser.tokenizer.next.type == "DIV":
-                if Parser.tokenizer.next.type == "MULT":
-                    Parser.tokenizer.selectNext()
-                    if Parser.tokenizer.next.value != None:
-                        num *= Parser.tokenizer.next.value
-                    else:
-                        raise Exception("Um numero deveria seguir token MULT")
-                if Parser.tokenizer.next.type == "DIV":
-                    Parser.tokenizer.selectNext()
-                    if Parser.tokenizer.next.value != None:
-                        num = num // Parser.tokenizer.next.value
-                    else:
-                        raise Exception("Um numero deveria seguir token DIV")
+            return resultado
+        elif Parser.tokenizer.next.type == "PLUS":
+            Parser.tokenizer.selectNext()
+            resultado = +Parser.parseFactor()
+            return resultado
+        elif Parser.tokenizer.next.type == "MINUS":
+            Parser.tokenizer.selectNext()
+            resultado = -Parser.parseFactor()
+            return resultado
+        elif Parser.tokenizer.next.type == "OPENPAR":
+            Parser.tokenizer.selectNext()
+            resultado = Parser.parseExpression()
+            if Parser.tokenizer.next.type == "CLOSEPAR":
                 Parser.tokenizer.selectNext()
-            return num
-        raise Exception("Primeiro token nao eh numero")
+                return resultado
+            raise Exception("Nao fechou parenteses")
+        else:
+           raise Exception("Nao foi factor") 
+
+    def parseTerm():
+        resultado = Parser.parseFactor() 
+        while Parser.tokenizer.next.type == "MULT" or Parser.tokenizer.next.type == "DIV":
+            if Parser.tokenizer.next.type == "MULT":
+                Parser.tokenizer.selectNext()
+                resultado *= Parser.parseFactor()
+            if Parser.tokenizer.next.type == "DIV":
+                Parser.tokenizer.selectNext()
+                resultado = resultado // Parser.parseFactor()
+        return resultado
 
     def parseExpression():
         resultado = Parser.parseTerm() 
