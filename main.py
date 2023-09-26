925fcaf1d455f852dd5f2d07c3740caa797886b4
TiagoSeixas2103 <tvseixas@gmail.com>
2023-09-26 12:45:11 -0300

    v2.2.0

--- main.py
+++ main.py
@@ -13,12 +13,6 @@ class PrePro:
     def filter(self):
         file = open(self.source, "r")
         linhas = file.readlines()
-        """ while self.position < len(self.source):
-            if self.source[self.position] == "/" and self.source[self.position + 1] == "/":
-                while self.position < len(self.source) and self.source[self.position] != "\n":
-                    self.source = self.source.split(self.source[self.position])[0] + self.source.split(self.source[self.position])[1]
-            else:
-                self.position += 1 """
         novas_linhas = []
         for linha in linhas:
             if "//" in linha:
@@ -33,10 +27,35 @@ class Tokenizer:
         self.source = str (source)
         self.position = int (position)
         self.next = Token("", None)
-        self.palavras_reservadas = {"Println" : "PRINT"}
+        self.palavras_reservadas = {
+            "Println" : "PRINT",
+            "Scanln" : "SCAN",
+            "if" : "IF",
+            "for" : "FOR",
+            "else" : "ELSE",
+        }
+        self.tipos = {
+            "+" : "PLUS",
+            "-" : "MINUS",
+            "*" : "MULT",
+            "/" : "DIV",
+            "(" : "OPENPAR",
+            ")" : "CLOSEPAR",
+            "\n" : "ENTER",
+            ">" : "GREATERTHAN",
+            "<" : "LOWERTHAN",
+            "!" : "NOT",
+            "{" : "OPENKEY",
+            "}" : "CLOSEKEY",
+            ";" : "SEMICOLON",
+        }
 
     def selectNext(self):
-        while self.position < len(self.source) and self.source[self.position] == " ":
+        whitespaces = {
+            " " : "SPACE",
+            "\t" : "TAB",
+        }
+        while self.position < len(self.source) and self.source[self.position] in whitespaces:
             self.position += 1
             
         if self.position < len(self.source):
@@ -49,24 +68,6 @@ class Tokenizer:
                     posicao +=1
                 self.next.value = int(string)
                 self.position = posicao - 1
-            elif self.source[self.position] == "+":
-                self.next.type = "PLUS"
-                self.next.value = None
-            elif self.source[self.position] == "-":
-                self.next.type = "MINUS"
-                self.next.value = None
-            elif self.source[self.position] == "*":
-                self.next.type = "MULT"
-                self.next.value = None
-            elif self.source[self.position] == "/":
-                self.next.type = "DIV"
-                self.next.value = None
-            elif self.source[self.position] == "(":
-                self.next.type = "OPENPAR"
-                self.next.value = None
-            elif self.source[self.position] == ")":
-                self.next.type = "CLOSEPAR"
-                self.next.value = None
             elif self.source[self.position].islower() or self.source[self.position].isupper():
                 posicao = self.position+1
                 string = self.source[self.position]
@@ -80,10 +81,25 @@ class Tokenizer:
                     self.next.type = "IDENTIFIER"
                 self.position = posicao - 1
             elif self.source[self.position] == "=":
-                self.next.type = "EQUAL"
-                self.next.value = None
-            elif self.source[self.position] == "\n":
-                self.next.type = "ENTER"
+                if self.source[self.position + 1] == "=":
+                    self.position += 1
+                    self.next.type = "EQUALTO"
+                    self.next.value = None
+                else:
+                    self.next.type = "EQUAL"
+                    self.next.value = None
+            elif self.source[self.position] == "&":
+                if self.source[self.position+1] == "&":
+                    self.position += 1
+                    self.next.type = "AND"
+                    self.next.value = None
+            elif self.source[self.position] == "|":
+                if self.source[self.position+1] == "|":
+                    self.position += 1
+                    self.next.type = "OR"
+                    self.next.value = None
+            elif self.source[self.position] in self.tipos.keys():
+                self.next.type = self.tipos[self.source[self.position]]
                 self.next.value = None
             else:
                 self.next.type = "ERROR"
@@ -124,6 +140,16 @@ class BinOp(Node):
             return self.children[0].Evaluate(SymbolTable) * self.children[1].Evaluate(SymbolTable)
         if self.value == "DIV":   
             return self.children[0].Evaluate(SymbolTable) // self.children[1].Evaluate(SymbolTable)
+        if self.value == "AND":   
+            return self.children[0].Evaluate(SymbolTable) and self.children[1].Evaluate(SymbolTable)
+        if self.value == "OR":   
+            return self.children[0].Evaluate(SymbolTable) or self.children[1].Evaluate(SymbolTable)
+        if self.value == "EQUALTO":   
+            return self.children[0].Evaluate(SymbolTable) == self.children[1].Evaluate(SymbolTable)
+        if self.value == "GREATERTHAN":   
+            return self.children[0].Evaluate(SymbolTable) > self.children[1].Evaluate(SymbolTable)
+        if self.value == "LOWERTHAN":   
+            return self.children[0].Evaluate(SymbolTable) < self.children[1].Evaluate(SymbolTable)
 
 class UnOp(Node):
     def Evaluate(self, SymbolTable):
@@ -158,6 +184,21 @@ class Println(Node):
     def Evaluate(self, SymbolTable):
         print(self.children[0].Evaluate(SymbolTable)) 
 
+class If(Node):
+    def Evaluate(self, SymbolTable):
+        if self.children[0].Evaluate(SymbolTable):
+            return self.children[1].Evaluate(SymbolTable)
+        else:
+            if len(self.children) > 2:
+                return self.children[2].Evaluate(SymbolTable)
+
+class For(Node):
+    def Evaluate(self, SymbolTable):
+        self.children[0].Evaluate(SymbolTable)
+        while (self.children[1].Evaluate(SymbolTable)):
+            self.children[3].Evaluate(SymbolTable)
+            self.children[2].Evaluate(SymbolTable)
+
 class Parser:
     tokenizer = Tokenizer("", 0, Token("", None))
     def parseFactor():       
@@ -177,13 +218,27 @@ class Parser:
             Parser.tokenizer.selectNext()
             resultado = UnOp("MINUS", [Parser.parseFactor()])
             return resultado
+        elif Parser.tokenizer.next.type == "NOT":
+            Parser.tokenizer.selectNext()
+            resultado = UnOp("NOT", [Parser.parseFactor()])
+            return resultado
         elif Parser.tokenizer.next.type == "OPENPAR":
             Parser.tokenizer.selectNext()
-            resultado = Parser.parseExpression()
+            resultado = Parser.parseBoolExpression()
             if Parser.tokenizer.next.type == "CLOSEPAR":
                 Parser.tokenizer.selectNext()
                 return resultado
             raise Exception("Nao fechou parenteses")
+        elif Parser.tokenizer.next.type == "SCAN":
+            Parser.tokenizer.selectNext()
+            if Parser.tokenizer.next.type == "OPENPAR":
+                Parser.tokenizer.selectNext()
+                if Parser.tokenizer.next.type == "CLOSEPAR":
+                    Parser.tokenizer.selectNext()
+                    resultado = IntVal(int(input()), None)
+                    return resultado
+                raise Exception("Nao fechou parenteses no scan")
+            raise Exception("Nao abriu parenteses no scan")
         else:
            raise Exception("Nao foi factor") 
 
@@ -209,6 +264,36 @@ class Parser:
                 resultado = BinOp("MINUS",[resultado, Parser.parseTerm()])
         return resultado
     
+    def parseRelationalExpression():
+        resultado = Parser.parseExpression()
+        condicao = Parser.tokenizer.next.type 
+        while condicao == "EQUALTO" or condicao == "GREATERTHAN" or condicao == "LOWERTHAN":
+            if Parser.tokenizer.next.type == "EQUALTO":
+                Parser.tokenizer.selectNext()
+                resultado = BinOp("EQUALTO",[resultado, Parser.parseExpression()])
+            if Parser.tokenizer.next.type == "GREATERTHAN":
+                Parser.tokenizer.selectNext()
+                resultado = BinOp("GREATERTHAN",[resultado, Parser.parseExpression()])
+            if Parser.tokenizer.next.type == "LOWERTHAN":
+                Parser.tokenizer.selectNext()
+                resultado = BinOp("LOWERTHAN",[resultado, Parser.parseExpression()])
+            condicao = Parser.tokenizer.next.type
+        return resultado
+
+    def parseBoolTerm():
+        resultado = Parser.parseRelationalExpression() 
+        while Parser.tokenizer.next.type == "AND":
+            Parser.tokenizer.selectNext()
+            resultado = BinOp("AND",[resultado, Parser.parseRelationalExpression()])
+        return resultado
+    
+    def parseBoolExpression():
+        resultado = Parser.parseBoolTerm() 
+        while Parser.tokenizer.next.type == "OR":
+            Parser.tokenizer.selectNext()
+            resultado = BinOp("OR",[resultado, Parser.parseBoolTerm()])
+        return resultado
+    
     def parseStatement():
         resultado = NoOp(None, [])
         if Parser.tokenizer.next.type == "ENTER":
@@ -220,30 +305,86 @@ class Parser:
             Parser.tokenizer.selectNext()
             if Parser.tokenizer.next.type == "EQUAL":
                 Parser.tokenizer.selectNext()
-                resultado = Assignment("EQUAL", [Identifier(valor_ident, None), Parser.parseExpression()])
+                resultado = Assignment("EQUAL", [Identifier(valor_ident, None), Parser.parseBoolExpression()])
                 if Parser.tokenizer.next.type == "ENTER":
                     Parser.tokenizer.selectNext()
                     return resultado
-                raise Exception("Nao deu Enter 1")
+                raise Exception("Nao deu Enter depois do assignment")
             raise Exception("Nao botou igual ou sinal")
         
         elif Parser.tokenizer.next.type == "PRINT":
             Parser.tokenizer.selectNext()
             if Parser.tokenizer.next.type == "OPENPAR":
                 Parser.tokenizer.selectNext()
-                resultado = Println("PRINT", [Parser.parseExpression()])
+                resultado = Println("PRINT", [Parser.parseBoolExpression()])
                 if Parser.tokenizer.next.type == "CLOSEPAR":
                     Parser.tokenizer.selectNext()
                     if Parser.tokenizer.next.type == "ENTER":
                         Parser.tokenizer.selectNext()
                         return resultado
-                    raise Exception("Nao deu Enter 2")
+                    raise Exception("Nao deu Enter depois do print")
                 raise Exception("Nao fechou parenteses")
             raise Exception("Nao abriu parenteses")
-        raise Exception("Nao usou uma funcionalidade")        
-
+        
+        elif Parser.tokenizer.next.type == "IF":
+            Parser.tokenizer.selectNext()
+            condition = Parser.parseBoolExpression()
+            block = Parser.parseBlock()
+            resultado = If("IF", [condition, block])
+            if Parser.tokenizer.next.type == "ELSE":
+                Parser.tokenizer.selectNext()
+                newBlock = Parser.parseBlock()
+                resultado.children.append(newBlock)
+            return resultado
+        
+        elif Parser.tokenizer.next.type == "FOR":
+            Parser.tokenizer.selectNext()
+            if Parser.tokenizer.next.type == "IDENTIFIER":
+                valor_ident_for = Parser.tokenizer.next.value
+                Parser.tokenizer.selectNext()
+                if Parser.tokenizer.next.type == "EQUAL":
+                    Parser.tokenizer.selectNext()
+                    init = Assignment("EQUAL", [Identifier(valor_ident_for, None), Parser.parseBoolExpression()])
+                    if Parser.tokenizer.next.type == "SEMICOLON":
+                        Parser.tokenizer.selectNext()
+                        condition_for = Parser.parseBoolExpression()
+                        if Parser.tokenizer.next.type == "SEMICOLON":
+                            Parser.tokenizer.selectNext()
+                            if Parser.tokenizer.next.type == "IDENTIFIER":
+                                increment_ident_for = Parser.tokenizer.next.value
+                                Parser.tokenizer.selectNext()
+                                if Parser.tokenizer.next.type == "EQUAL":
+                                    Parser.tokenizer.selectNext()
+                                    increment = Assignment("EQUAL", [Identifier(increment_ident_for, None), Parser.parseBoolExpression()])
+                                    block_for = Parser.parseBlock()
+                                    resultado = For("FOR", [init, condition_for, increment, block_for])
+                                    return resultado
+                                raise Exception("Faltou atribuir valor incremento do for")
+                            raise Exception("Faltou incremento do for")
+                        raise Exception("Faltou ponto e virgula depois do condition do for")
+                    raise Exception("Faltou ponto e virgula depois do init do for")
+                raise Exception("Nao atribuiu valor no init do for")
+            raise Exception("Sem init do for")
+        
+        raise Exception("Nao usou uma funcionalidade") 
+            
 
     def parseBlock():
+        if Parser.tokenizer.next.type == "OPENKEY":
+            Parser.tokenizer.selectNext()
+            if Parser.tokenizer.next.type == "ENTER":
+                Parser.tokenizer.selectNext()
+                resultado = Block(None, [])
+                while Parser.tokenizer.next.type != "CLOSEKEY":
+                    resultado.children.append(Parser.parseStatement())
+                if Parser.tokenizer.next.type == "CLOSEKEY":
+                    Parser.tokenizer.selectNext()
+                    return resultado
+                raise Exception("Nao fechou chaves")
+            raise Exception("Nao deu enter depois de abrir chaves")
+        raise Exception("Nao abriu chaves")
+
+    def parseProgram():
         resultado = Block(None, [])
         while Parser.tokenizer.next.type != "EOF":
             resultado.children.append(Parser.parseStatement())
@@ -255,7 +396,7 @@ class Parser:
         texto = ProCode.filter()
         Parser.tokenizer.source = texto
         Parser.tokenizer.selectNext()
-        resultado = Parser.parseBlock()
+        resultado = Parser.parseProgram()
         if Parser.tokenizer.next.type != "EOF":
             raise Exception("Deveria ser o fim da string")
         return resultado
