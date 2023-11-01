24a0daf961c6b4f3cbbfe36a811de7db0049c506
Tiago Seixas <tvseixas@gmail.com>
2023-11-01 11:12:29 -0300

    v3.0.5

--- endasm.py
+++ endasm.py
@@ -1,16 +1,8 @@
 class FinalizeASM:
-    list_init = [
-        "\n",
-        "; depois que terminar de gerar o código:\n",
-        "PUSH DWORD [stdout]\n",
-        "CALL fflush\n",
-        "ADD ESP, 4\n",
-        "MOV ESP, EBP\n",
-        "POP EBP\n",
-        "MOV EAX, 1\n",
-        "XOR EBX, EBX\n",
-        "INT 0x80\n",
-    ]
+    list_init = []
+    with open("end.asm") as file:
+        for line in file:
+            list_init.append(line)
     def writeEndASM(program):
         with open(program, "a") as assemblyFile:
             for string in FinalizeASM.list_init:
--- initasm.py
+++ initasm.py
@@ -1,50 +1,8 @@
 class InitializeASM:
-    list_init = [
-        "; constantes\n", "SYS_EXIT equ 1\n", 
-        "SYS_READ equ 3\n", 
-        "SYS_WRITE equ 4\n", 
-        "STDIN equ 0\n", 
-        "STDOUT equ 1\n", 
-        "True equ 1\n", 
-        "False equ 0\n", 
-        "\n", 
-        "segment .data\n", 
-        'formatin: db "%d", 0\n', 
-        'formatout: db "%d", 10, 0 ; newline, null terminator\n', 
-        "scanint: times 4 db 0 ; 32-bit integer = 4 bytes\n", 
-        "\n", 
-        "segment .bss ; variáveis\n", 
-        "res RESB 1\n", 
-        "\n", 
-        "section .text\n", 
-        "global main ; linux\n", 
-        "; global _main ; windows\n", 
-        "extern scanf ; linux\n", 
-        "extern printf ; linux\n", 
-        "; extern _scanf ; windows\n", 
-        "extern _printf ; windows\n", 
-        "\n", 
-        "; subrotinas if/while\n", 
-        "binop_je:\n", 
-        "    JE binop_true\n", 
-        "    JMP binop_false\n", 
-        "binop_jg:\n", 
-        "    JG binop_true\n", 
-        "    JMP binop_false\n", 
-        "binop_jl:\n", 
-        "    JL binop_true\n", 
-        "    JMP binop_false\n", 
-        "binop_false:\n", 
-        "    MOV EAX, False\n", 
-        "    JMP binop_exit\n", 
-        "binop_true:\n", 
-        "    MOV EAX, True\n", 
-        "binop_exit:\n", 
-        "    RET\n", 
-        "\n", 
-        "main:\n", 
-        "\n" 
-    ]
+    list_init = []
+    with open("init.asm") as file:
+        for line in file:
+            list_init.append(line)
     def writeInitASM(program):
         with open(program, "a") as assemblyFile:
             for string in InitializeASM.list_init:
